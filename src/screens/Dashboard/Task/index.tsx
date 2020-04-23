29e5afa61b626f3c59a0672c76a549fdae110e47
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-04-23 02:09:38 -0400

    Add User id

--- src/screens/Dashboard/Task/index.tsx
+++ src/screens/Dashboard/Task/index.tsx
@@ -11,7 +11,7 @@ const Task = ({ id }) => {
   const user = useContext(UserContext)
   const [ isMember, setIsMember ] = useState(false)
   const where = id.length > 6 ? { id } : { code: id }
-  const { data, loading, error } = useQuery(TASK, { variables: { where } })
+  const { data, loading, error } = useQuery(TASK, { variables: { where, userId: user.id } })
   useEffect(()= > {
     if(data && data.task && data.task.organization && data.task.organization.users > 0) {
       setIsMember(true)
