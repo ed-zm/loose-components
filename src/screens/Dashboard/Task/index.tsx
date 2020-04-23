f2db468861399535ff09715159c124a15ba93061
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-04-23 02:07:19 -0400

    Add user context abd isMember Variable

--- src/screens/Dashboard/Task/index.tsx
+++ src/screens/Dashboard/Task/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import moment from 'moment'
 import { useQuery } from '@apollo/react-hooks'
 import Assign from './components/Assign'
 import Labels from './components/Labels'
 import Comments from './components/Comments'
 import { TASK } from './index.graphql'
+import { UserContext } from '../../../contexts/User'
 
 const Task = ({ id }) => {
+  const user = useContext(UserContext)
+  const [ isMember, setIsMember ] = useState(false)
   const where = id.length > 6 ? { id } : { code: id }
   const { data, loading, error } = useQuery(TASK, { variables: { where } })
+  useEffect(()= > {
+    if(data && data.task && data.task.organization && data.task.organization.users > 0) {
+      setIsMember(true)
+    }
+  }, [data])
   return({
     data,
     loading,
-    error
+    error,
+    isMember
   })
 }
 
