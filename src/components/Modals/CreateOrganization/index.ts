4e51c1ae2b0a44bed8cb54b49a08d1efa8481367
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2020-11-11 08:02:56 -0400

    add error message to create organization modal

--- src/components/Modals/CreateOrganization/index.ts
+++ src/components/Modals/CreateOrganization/index.ts
@@ -6,43 +6,50 @@ import { UserContext } from '../../../contexts/User'
 
 const CreateOrganization = ({ variables }) => {
   const user = useContext(UserContext)
-  const [ createOrganization ] = useMutation(CREATE_ORGANIZATION)
+  const [ createOrganization, { loading: creatingOrganization } ] = useMutation(CREATE_ORGANIZATION)
   const [ name, setName ] = useState('')
   const [plan, setPlan] = useState("");
+  const [  error, setError ] = useState("")
   const onCreateOrganization = async (token) => {
-    createOrganization({ variables: {
-      name,
-      userId: user.id,
-      stripeId: token.id
-    },
-    optimisticResponse: {
-      __typename: 'Mutation',
-      createOneOrganization: {
-        __typename: "Organization",
-        id: "-1",
-        name,
-        owner: {
-          id: user.id,
-          __typename: "User"
-        },
-        users: [
-          {
-            id: user.id,
-            firstName: user.firstName,
-            avatar: user.avatar,
-            __typename: "User"
-          }
-        ],
-        teams: []
-      }
-    },
-    update: (proxy, { data: { createOneOrganization }}) => {
-      const data = proxy.readQuery({ query: ORGANIZATIONS, variables })
-      const newOrganizations = data.organizations.slice()
-      newOrganizations.unshift(createOneOrganization)
-      proxy.writeQuery({ query: ORGANIZATIONS, variables, data: { organizations: newOrganizations }})
+    if(name.length < 3) {
+      await setError('Organization name must be at least 3 characters long')
     }
-  })
+    else {
+      createOrganization({
+        variables: {
+          name,
+          userId: user.id,
+          stripeId: token.id
+        },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          createOneOrganization: {
+            __typename: "Organization",
+            id: "-1",
+            name,
+            owner: {
+              id: user.id,
+              __typename: "User"
+            },
+            users: [
+              {
+                id: user.id,
+                firstName: user.firstName,
+                avatar: user.avatar,
+                __typename: "User"
+              }
+            ],
+            teams: []
+          }
+        },
+        update: (proxy, { data: { createOneOrganization }}) => {
+        const data = proxy.readQuery({ query: ORGANIZATIONS, variables })
+        const newOrganizations = data.organizations.slice()
+        newOrganizations.unshift(createOneOrganization)
+        proxy.writeQuery({ query: ORGANIZATIONS, variables, data: { organizations: newOrganizations }})
+      }
+      }).catch(e => setError("We could not create the organization"))
+    }
   }
   return {
     onCreateOrganization,
@@ -50,6 +57,9 @@ const CreateOrganization = ({ variables }) => {
     setName,
     plan,
     setPlan,
+    error,
+    setError,
+    creatingOrganization
   }
 }
 
